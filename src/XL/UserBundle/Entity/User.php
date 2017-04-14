ffa8bcde7307947d14cd275af9bbb6a94e467dd5
Unknown <xavier.loire@epitech.eu>
2017-04-14 12:04:29 +0200

    Start project / Inscription, Connection, Déconnection avec FOSUserBundle

---
+++ src/XL/UserBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace XL\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="bonobo_users")
+ */
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
---
+++ src/XL/UserBundle/XLUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace XL\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class XLUserBundle extends Bundle
+{
+}
